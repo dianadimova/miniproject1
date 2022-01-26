44f1316199cb3a4c765f0e2fb23dc5704b99ca90
dianadimova <ddimova1818@gmail.com>
2022-01-26 00:11:20 -0600

    this is my final version

--- sketch.js
+++ sketch.js
@@ -3,8 +3,43 @@ function setup() {
   }
   
   function draw() {
-    background(220);
+   //background(220,220,220);
+   background(255,255,255);
 
-    strokeWeight(25);
-    circle(200,200,500);
+    //stroke(0,0,0);
+    //strokeWeight(25);
+    //circle(200,200,500);
+
+    //strokeWeight(25);
+   //circle(1300,650,300);
+   
+   //stroke(255,105,180);
+   //line(1000,0,1000,840);
+    //line(800,0,800,840);
+    //line(600,0,600,840);
+
+    fill(0,0,0);
+    stroke(10);
+    rect(240,450,1000,400);
+
+    noFill();
+    stroke(10);
+    rect(240,50,1000,400);
+
+    fill(255,255,0);
+    noStroke();
+    rect(340,150,200,600);
+
+    fill(255,0,0);
+    noStroke();
+    rect(640,150,200,600);
+
+    fill(0,0,255)
+    noStroke();
+    rect(940,150,200,600);
+
+    
+    
+
+    
   }
